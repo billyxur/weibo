bc01bc5d0e71e5623ed3026688d47398342b92c5
billyxu <billyxuxyxbb@gmail.com>
2019-04-05 16:03:47 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,10 +7,20 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        //只让未登录用户访问登录页面：
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+
     public function create()
     {
         return view('sessions.create');
     }
+
 
     public function store(Request $request)
     {
@@ -22,7 +32,8 @@ class SessionsController extends Controller
        if (Auth::attempt($credentials, $request->has('remember'))) {
             //登录成功
            session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
        } else {
             //登录失败
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
@@ -30,6 +41,7 @@ class SessionsController extends Controller
        }
 
     }
+
 
     public function destroy()
     {
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,15 +8,31 @@ use Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'expect' => ['show', 'create', 'store']
+        ]);
+
+        //只让未登录用户访问注册页面：
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+
     public function create()
     {
         return view('users.create');
     }
 
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
+
 
     public function store(Request $request)
     {
@@ -38,13 +54,17 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
+
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
+
     public function update(User $user, Request $request)
     {
+         $this->authorize('update', $user);
          $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
