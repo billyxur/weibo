71f9de63261fb420d8b17011590bb84e9fa4a727
billyxu <billyxuxyxbb@gmail.com>
2019-04-09 16:20:52 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -143,4 +143,21 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
+
+    //渲染用户关注的人页面
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    //渲染粉丝信息页面
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
 }
